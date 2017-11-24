14822c3fcb3bce7e16fc7ca8299f6b9e74754383
Viktor Adam <rycus86@gmail.com>
2017-11-24 23:04:18 +0000

    Prepare next version number

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ except IOError:
 setup(
     name='prometheus_flask_exporter',
     packages=['prometheus_flask_exporter'],
-    version='0.0.4',
+    version='0.0.5',
     description='Prometheus metrics exporter for Flask',
     long_description=long_description,
     license='MIT',
     author='Viktor Adam',
     author_email='rycus86@gmail.com',
     url='https://github.com/rycus86/prometheus-flask-exporter',
-    download_url='https://github.com/rycus86/prometheus-flask-exporter/archive/0.0.4.tar.gz',
+    download_url='https://github.com/rycus86/prometheus-flask-exporter/archive/0.0.5.tar.gz',
     keywords=['prometheus', 'flask', 'monitoring', 'exporter'],
     classifiers=[
         'Development Status :: 4 - Beta',
