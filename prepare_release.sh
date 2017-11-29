7b4e5ba1be519927c9329b2547644bb4d9c4af93
Viktor Adam <rycus86@gmail.com>
2017-11-29 23:49:58 +0000

    Bump version number

--- prepare_release.sh
+++ prepare_release.sh
@@ -8,5 +8,5 @@ fi
 VERSION="$1"
 
 sed -i "s/version=.*,/version='${VERSION}',/" setup.py
-sed -i "s#download_url=.*,#download_url='https://github.com/rycus86/prometheus-flask-exporter/archive/${VERSION}.tar.gz',#" setup.py
+sed -i "s#download_url=.*,#download_url='https://github.com/rycus86/prometheus_flask_exporter/archive/${VERSION}.tar.gz',#" setup.py
 sed -i "s/__version__ = '.*'/__version__ = '${VERSION}'/" prometheus_flask_exporter/__init__.py
