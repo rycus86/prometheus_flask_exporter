7d7cc44521c8eca6dd258dda4f448fa077195b21
Viktor Adam <rycus86@gmail.com>
2020-07-22 23:52:55 +1000

    Allow lazy api with app factory in RESTfulPrometheusMetrics.for_app_factory
    Fixes #62

--- examples/restful-return-none/server.py
+++ examples/restful-return-none/server.py
@@ -5,7 +5,7 @@ from prometheus_flask_exporter import RESTfulPrometheusMetrics
 app = Flask(__name__)
 restful_api = Api(app)
 
-metrics = RESTfulPrometheusMetrics.for_app_factory(restful_api)
+metrics = RESTfulPrometheusMetrics.for_app_factory()
 
 
 class Test(Resource):
@@ -26,5 +26,5 @@ restful_api.add_resource(Test, '/api/v1/test', endpoint='test')
 
 
 if __name__ == '__main__':
-    metrics.init_app(app)
+    metrics.init_app(app, restful_api)
     app.run('0.0.0.0', 4000)
