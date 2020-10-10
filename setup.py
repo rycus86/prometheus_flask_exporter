8ca7db638e7c723df509898dcf07fd27815d0e55
Viktor Adam <rycus86@gmail.com>
2020-10-10 20:28:04 +1100

    Python 3.9 compatibility

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as readme:
 setup(
     name='prometheus_flask_exporter',
     packages=['prometheus_flask_exporter'],
-    version='0.18.0',
+    version='0.18.1',
     description='Prometheus metrics exporter for Flask',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -14,7 +14,7 @@ setup(
     author='Viktor Adam',
     author_email='rycus86@gmail.com',
     url='https://github.com/rycus86/prometheus_flask_exporter',
-    download_url='https://github.com/rycus86/prometheus_flask_exporter/archive/0.18.0.tar.gz',
+    download_url='https://github.com/rycus86/prometheus_flask_exporter/archive/0.18.1.tar.gz',
     keywords=['prometheus', 'flask', 'monitoring', 'exporter'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -29,7 +29,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
     install_requires=['prometheus_client', 'flask'],
 )
