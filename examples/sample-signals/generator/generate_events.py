08c763b1b1aa5b92086d7c4b19fd5354d51c2cd0
Denis Trofimov <denis.trofimov@huma.com>
2022-02-03 18:02:34 +0300

    chore: add debug print
    Signed-off-by: Denis Trofimov <denis.trofimov@huma.com>

--- examples/sample-signals/generator/generate_events.py
+++ examples/sample-signals/generator/generate_events.py
@@ -1,23 +1,23 @@
 import time
 import random
 import threading
-
 import requests
 
-endpoints = ('one', 'two', 'three', 'four', 'error')
+endpoints = ("one", "two", "three", "four", "error")
+HOST = "http://app:5000/"
 
 
 def run():
     while True:
         try:
             target = random.choice(endpoints)
-            requests.get("http://app:5000/%s" % target, timeout=1)
-
+            requests.get(HOST + target, timeout=1)
         except:
-            pass
+            print("cannot connect", HOST)
+            time.sleep(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for _ in range(4):
         thread = threading.Thread(target=run)
         thread.daemon = True
