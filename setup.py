17896008c9db1df1f5c3b65a10d581fbe4f001c8
Viktor Adam <rycus86@gmail.com>
2023-02-21 11:47:34 +1000

    Release version 0.22.1

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
 from pathlib import Path
+from setuptools import setup
 
 long_description = Path('README.md').read_text()
 
 setup(
     name='prometheus_flask_exporter',
     packages=['prometheus_flask_exporter'],
-    version='0.22.0',
+    version='0.22.1',
     description='Prometheus metrics exporter for Flask',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -14,7 +14,7 @@ setup(
     author='Viktor Adam',
     author_email='rycus86@gmail.com',
     url='https://github.com/rycus86/prometheus_flask_exporter',
-    download_url='https://github.com/rycus86/prometheus_flask_exporter/archive/0.22.0.tar.gz',
+    download_url='https://github.com/rycus86/prometheus_flask_exporter/archive/0.22.1.tar.gz',
     keywords=['prometheus', 'flask', 'monitoring', 'exporter'],
     classifiers=[
         'Development Status :: 4 - Beta',
