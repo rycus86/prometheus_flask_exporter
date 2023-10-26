8420d7ea75c5cdb42e187b68530ca3b454b405fd
Viktor Adam <rycus86@gmail.com>
2023-10-27 09:18:01 +1000

    Drop Python 3.7 support and start testing on 3.12

--- setup.py
+++ setup.py
@@ -23,11 +23,11 @@ setup(
         'Topic :: System :: Monitoring',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     install_requires=['prometheus_client', 'flask'],
 )
